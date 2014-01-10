641ad63ad278ac124dc740eacd636b319ff157c2
linzhu <linzhu0831@gmail.com>
2014-01-10 17:46:18 +0800

    Initial Commit

---
+++ ALRefreshTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ALRefreshTable
+//
+//  Created by lin zhu on 14-1-10.
+//  Copyright (c) 2014年 lin zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ALRefreshTableTests/ALRefreshTableTests.m
@@ -0,0 +1,34 @@
+//
+//  ALRefreshTableTests.m
+//  ALRefreshTableTests
+//
+//  Created by lin zhu on 14-1-10.
+//  Copyright (c) 2014年 lin zhu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ALRefreshTableTests : XCTestCase
+
+@end
+
+@implementation ALRefreshTableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
