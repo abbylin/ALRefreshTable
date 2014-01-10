cbbe5f2c6841cdd61c8cb670a2c265409fc52891
linzhu <linzhu0831@gmail.com>
2014-01-10 18:00:09 +0800

    test

--- ALRefreshTable/AppDelegate.m
+++ ALRefreshTable/AppDelegate.m
@@ -16,6 +16,8 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    //test
     return YES;
 }
 
